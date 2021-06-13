ea460534ce08c89df235b596d7f8f76c92fd7a17
manish kumar <aimaanish1503@gmail.com>
2021-06-13 08:03:32 +0530

    useQuery used in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import Launches from './components/Launches';
-import ApolloClient from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import sx from './sx.png'
 
 const client = new ApolloClient({
-uri: 'http://localhost:5000/graphql'
+uri: 'http://localhost:5000/graphql',
+cache: new InMemoryCache()
 })
 
 
 function App() {
   return (
     <ApolloProvider client={client} >
-    <div className="container">
-     <img 
-      src={sx} 
-      alt="spaceX"
-      style={{width:300,
-      display: 'block', 
-      margin: 'auto'}}/>
-      <Launches />
-    </div>
+      <div className="container">
+        <img 
+          src={sx} 
+          alt="spaceX"
+          style={{width:300,
+          display: 'block', 
+          margin: 'auto'}}/>
+        <Launches />
+      </div>
     </ApolloProvider>
   );
 }
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,43 +1,62 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
-import {Query} from 'react-apollo'
+//import {Query} from 'react-apollo'// uncomment when needed preivious version of Query
 import LaunchItem from './LaunchItem'
+import {
+    useQuery,
+  } from "@apollo/client";
+
 
  const LAUNCHES_QUERY = gql`
-query LaunchesQuery {
-    launches {
-        flight_number
-        mission_name
-        launch_date_local
-        launch_success
-    }
+    query LaunchesQuery {
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
 }
  `
 
-export class Launches extends Component {
-    render() {
-        return (
-            <>
-                <h1 className="display-4 my-3">Launches</h1>
-                <Query query={LAUNCHES_QUERY}>
-                    {
-                        ({ loading, error, data }) => {
-                            if(loading) return <h3>Loading...</h3>
-                            if(error) console.log(error)
-                            return <>
-                            {
-                                data.launches.map((launch, i) => (
+const Launches = () => {
+    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+        return <>
+                {data.launches.map((launch, i) => (
                                     <LaunchItem key={i} launch = {launch}/>
-                                ))
-                            }
-                            </>
-                        }
-                    }
+                                ))}
+               </>
+  }
 
-                </Query>
-            </>
-        )
-    }
-}
+// uncomment when using previous version
+
+// export class Launches extends Component {
+    
+//     render() {
+//         return (
+//             <>
+//                 <h1 className="display-4 my-3">Launches</h1>
+//                 <Query query={LAUNCHES_QUERY}>
+//                     {
+//                         ({ loading, error, data }) => {
+//                             if(loading) return <h3>Loading...</h3>
+//                             if(error) console.log(error)
+//                             return <>
+//                             {
+//                                 data.launches.map((launch, i) => (
+//                                     <LaunchItem key={i} launch = {launch}/>
+//                                 ))
+//                             }
+//                             </>
+//                         }
+//                     }
+
+//                 </Query>
+//             </>
+//         )
+//     }
+// }
 
 export default Launches
